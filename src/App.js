d24acde841889db43ffbc2330da9b1725ad25e8b
Blake DeGraw <bbdegraw@gmail.com>
2021-04-07 17:30:32 -0700

    finished 'about me', still no real formatting, just wireframe

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css'
+import Portrait from './images/portrait.jpg'
+import Resume from './Blake_DeGraw_resume.pdf'
 
 function App() {
   return (
@@ -11,6 +13,18 @@ function App() {
       </div>
       <div className="fade-in">
         <p id="blurb">software engineer | game developer | audio artist</p>
+      </div>
+      <div id="about-container">
+        <div id="left">
+          <img id="portrait" src={Portrait}></img>
+        </div>
+        <div id="right">
+          <div id="about-me">
+          I am a full-stack web developer with a passion for crafting unique user experiences. As an engineer, video game developer, and critically-acclaimed composer, my works challenge the user to interact with devices in unorthodox ways.
+          <br></br>
+          <a href={Resume} download target="_blank">resume</a> | <a href="https://www.linkedin.com/in/blake-degraw/"  target="_blank">linkedIn</a> | <a href="https://github.com/Blake-D"  target="_blank">GitHub</a> | <a href="mailto:bbdegraw@gmail.com" target="_blank">bbdegraw@gmail.com</a>
+          </div>
+        </div>
       </div>
     </>
   );
