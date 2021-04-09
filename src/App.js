f20642d0eac45cbec93e306205201aa4337daa68
Blake DeGraw <bbdegraw@gmail.com>
2021-04-09 11:52:20 -0700

    tech stack section added, linkedIn and github links replaced with icons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css'
 import Portrait from './images/portrait.jpg'
 import Resume from './Blake_DeGraw_resume.pdf'
+import LinkedIn from './images/linkedin_icon.png'
+import GitHub from './images/github_icon.png'
 
 function App() {
 
@@ -22,13 +24,29 @@ function App() {
       <div id="ribbon"></div>
       <div id="about-container">
         <div id="left">
-          <img id="portrait" src={Portrait}></img>
+          <img id="portrait" src={Portrait} alt="portrait of Blake"></img><br></br>
+          <div id="link-icons">
+            <div id="icon-left">
+              <a href="https://www.linkedin.com/in/blake-degraw/" target="_blank" rel="noreferrer"><img className="icon" src={LinkedIn} alt="linkedIn icon"/></a>
+            </div>
+            <div id="icon-right">
+              <a href="https://github.com/Blake-D" target="_blank" rel="noreferrer"><img className="icon" src={GitHub} alt="GitHub icon"/></a>
+            </div>
+          </div>
         </div>
         <div id="right">
           <div id="about-me">
-          I am a full-stack web developer with a passion for crafting unique user experiences. As an engineer, video game developer, and critically-acclaimed composer, my works challenge the user to interact with devices in unorthodox ways.
+            I am a full-stack web developer with a passion for crafting unique user experiences. As an engineer, video game developer, and critically-acclaimed composer, my works challenge the user to interact with devices in unorthodox ways.
+          <br></br><br></br>
+            <a href={Resume} target="_blank" rel="noreferrer">resume</a> |  <a href="mailto:bbdegraw@gmail.com" target="_blank" rel="noreferrer">bbdegraw@gmail.com</a>
+          </div>
           <br></br>
-          <a href={Resume} download target="_blank">resume</a> | <a href="https://www.linkedin.com/in/blake-degraw/"  target="_blank">linkedIn</a> | <a href="https://github.com/Blake-D"  target="_blank">GitHub</a> | <a href="mailto:bbdegraw@gmail.com" target="_blank">bbdegraw@gmail.com</a>
+          <div id="tech-stacks">
+            JavaScript <b>|</b> ES6 <br></br>
+            HTML5 <b>|</b> CSS <b>|</b> Bootstrap <b>|</b> Responsive Web Design <br></br>
+            React <b>|</b> Next <b>|</b> Node <b>|</b> Express <b>|</b> EJS <b>|</b> RESTful Routing <br></br>
+            Python <b>|</b> Flask <br></br>
+            SQL <b>|</b> PostgreSQL <b>|</b> Sequelize <b>|</b> MongoDB <b>|</b> Mongoose <b>|</b> Atlas
           </div>
         </div>
       </div>
