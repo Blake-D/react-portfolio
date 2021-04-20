fbec50e4c8bf7a680a9801defb0e50e4bf7a398a
Blake DeGraw <bbdegraw@gmail.com>
2021-04-20 11:02:16 -0700

    routine push. designing mobile version

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ import Resume from '../Blake_DeGraw_resume.pdf'
 function Header(){
     return(
         <div id="nav">
-            <a href="https://www.linkedin.com/in/blake-degraw/" target="_blank" rel="noreferrer"><img className="icon" src={LinkedIn} alt="linkedIn icon" /></a> <a href="https://github.com/Blake-D" target="_blank" rel="noreferrer"><img className="icon" src={GitHub} alt="GitHub icon" /></a> <a href="#cube-1">apps</a> | <a href="#cube-2">games</a> | <a id="contact-info" href={Resume} target="_blank" rel="noreferrer">resume</a> | <a href="mailto:bbdegraw@gmail.com" target="_blank" rel="noreferrer">contact me</a>
+            <a href="https://www.linkedin.com/in/blake-degraw/" target="_blank" rel="noreferrer"><img className="icon" src={LinkedIn} alt="linkedIn icon" /></a> <a href="https://github.com/Blake-D" target="_blank" rel="noreferrer"><img className="icon" src={GitHub} alt="GitHub icon" /></a> <a className="nav-link" href="#cube-1">apps | </a> <a className="nav-link" href="#cube-2">games | </a> <a className="nav-link" id="contact-info" href={Resume} target="_blank" rel="noreferrer">resume | </a> <a className="nav-link" href="mailto:bbdegraw@gmail.com" target="_blank" rel="noreferrer">contact me</a>
       </div>
     )
 }
