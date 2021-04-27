d6a1fe3a7ab961d4ee5af60c18826a8200571a77
Blake DeGraw <bbdegraw@gmail.com>
2021-04-27 15:56:31 -0700

    fixed 12 tone title problem

--- src/components/Matrix.js
+++ src/components/Matrix.js
@@ -4,7 +4,7 @@ import MatrixPic from '../images/matrix_screenshot.png'
 function Matrix(){
     return(
         <div className="cube-div">
-            <h1 className="two-d">Web Oscillator</h1>
+            <h1>12-Tone Matrix Player</h1>
             <img className="screenshot" src={MatrixPic} alt="12-Tone Matrix screenshot"/>
         </div>
     )
