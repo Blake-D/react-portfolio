01f6ed9e5eb5e777fc3d2d12848733dec8693525
Blake DeGraw <bbdegraw@gmail.com>
2021-05-13 15:17:16 -0700

    updated web oscillator info

--- src/components/OscText.js
+++ src/components/OscText.js
@@ -5,8 +5,8 @@ function OscText() {
     return (
         <div className="cube-div">
             <div className="about">
-                <b>Web Oscillator</b> is an online synthesizer written in Javascript, CSS, and HTML. The user can produce a drone of their choosing and manipulate its frequency and harmony by dragging the cursor across the page's X and Y axes.<br></br>
-                <a className="cube-link" href="https://blake-d.github.io/web-oscillator/" target="_blank" rel="noreferrer">launch app</a><br></br>
+                <b>Web Oscillator</b> is an online synthesizer made in React. The user can produce a drone of their choosing and manipulate its frequency and harmony by dragging the cursor across the page's X and Y axes.<br></br>
+                <a className="cube-link" href="https://web-oscillator.netlify.app/" target="_blank" rel="noreferrer">launch app</a><br></br>
                 <a className="cube-link" href="https://github.com/Blake-D/web-oscillator" target="_blank" rel="noreferrer">GitHub repo</a>
             </div>
         </div>
