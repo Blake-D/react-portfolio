24d8892f5157fb37ce2ac58a9322770238ca2c8a
Blake DeGraw <bbdegraw@gmail.com>
2021-05-14 13:15:43 -0700

    changed web-osc link in mobile version to take you to the mobile version of web-osc

--- src/components/OscMobile.js
+++ src/components/OscMobile.js
@@ -8,7 +8,7 @@ function OscMobile(){
             <img className="screenshot" src={OscPic} alt="Web Oscillator screenshot"/>
             <div className="about">
                 <b>Web Oscillator</b> is an online synthesizer made in React. The user can produce a drone of their choosing and manipulate its frequency and harmony by dragging the cursor across the page's X and Y axes.<br></br>
-                <a className="cube-link" href="https://web-oscillator.netlify.app/" target="_blank" rel="noreferrer">launch app</a><br></br>
+                <a className="cube-link" href="https://blake-d.github.io/web-oscillator-mobile/" target="_blank" rel="noreferrer">launch app</a><br></br>
                 <a className="cube-link" href="https://github.com/Blake-D/web-oscillator" target="_blank" rel="noreferrer">GitHub repo</a><br></br><br></br>
             </div>
         </div>
