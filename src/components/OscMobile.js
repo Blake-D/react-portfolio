a021bbc095a3b2094dbffb30fb1e26514c1cd874
Blake DeGraw <bbdegraw@gmail.com>
2021-05-14 14:01:49 -0700

    updated githib repo link for web-osc mobile

--- src/components/OscMobile.js
+++ src/components/OscMobile.js
@@ -9,7 +9,7 @@ function OscMobile(){
             <div className="about">
                 <b>Web Oscillator</b> is an online synthesizer made in React. The user can produce a drone of their choosing and manipulate its frequency and harmony by dragging the cursor across the page's X and Y axes.<br></br>
                 <a className="cube-link" href="https://blake-d.github.io/web-oscillator-mobile/" target="_blank" rel="noreferrer">launch app</a><br></br>
-                <a className="cube-link" href="https://github.com/Blake-D/web-oscillator" target="_blank" rel="noreferrer">GitHub repo</a><br></br><br></br>
+                <a className="cube-link" href="https://github.com/Blake-D/web-oscillator-mobile" target="_blank" rel="noreferrer">GitHub repo</a><br></br><br></br>
             </div>
         </div>
     )
